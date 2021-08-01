f6c7250bd58e1d5a9a1075d73a40308a2d4d443a
BXBotz-MufazTG <87131752+BXBotz-MufazTG@users.noreply.github.com>
2021-08-01 11:52:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -18,8 +18,7 @@ class Config(object):
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	ABOUT_TEXT = f"""
-🤖 **Bot** : File Store Bot
+	ABOUT_MSG = f"""🤖 **Bot** : File Store Bot
 
 👲 **Developer** : [ᴍʜᴅ ᴍᴜꜰᴀz](https://telegram.me/Mufaz123)
 
@@ -38,14 +37,12 @@ class Config(object):
 	ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 **Developer:[ᴍʜᴅ ᴍᴜꜰᴀᴢ](https://t.me/mufaz123)
 """
-	HOME_TEXT = """
-Hai, [{}](tg://user?id={})
+	HOME_TEXT = """Hai, [{}](tg://user?id={})
 
 `Iam a Simple File Store Bot.
 Send me any file I will give you permenent sharable link`
 
 👲 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : [ʙx ʙᴏᴛᴢ](https://t.me/BX_Botz)
 """
-	HELP_TEXT = f"""
-Join My Update Channel @BX_Botz
+	HELP_TEXT = f"""Join My Update Channel @BX_Botz
 """
